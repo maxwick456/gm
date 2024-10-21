309b7bf1e895b398cb27320571944319b243fe89
Max <maxwick456@gmail.com>
2024-10-21 12:45:39 -0600

    Update games.js

--- games.js
+++ games.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/cjs/sw.js')
+        navigator.serviceWorker.register('/sw.js')
             .then(registration => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
